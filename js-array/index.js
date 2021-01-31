61add78714e2d62fa1ceb12ef1ad97f32eb8e898
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-01-31 18:24:35 +0530

    JS Array | Accessing Array | Array Method

---
+++ js-array/index.js
@@ -0,0 +1,33 @@
+// declare and assign
+var list = ["Book", "Pen", "Mobile", "Guitar"]
+console.log("list = ", list);
+
+// Another Method to declare and assign value to array using new keyword
+var cars = new Array("Volvo", "Hyundai", "Honda");
+console.log("cars = ", cars);
+
+// Access the element of an array // Index starts from 0
+var firstCarName = cars[0]
+console.log("firstCarName = ", firstCarName);
+
+var namelist = list[2]
+console.log("namelist = ", namelist);
+
+const arr = ["Samsung", "Redmi", "Oppo", "Vivo"]
+console.log("arr = ", arr);
+
+arr[1] = "Mi";
+console.log("Modified Arr = ", arr); 
+console.log(typeof(arr)); // Array is treated as object in JS
+
+// Array with multiple data types
+var arr1 = ["Information", {"Name" : "Shani Kumar Gupta", "Age" : 21, "College" : "GLA University"}, 34, 34.56]
+console.log("Array Objects = ", arr1);
+console.log("Info = ", arr1[1].Name);
+
+// Array method
+// 1. length : used to get the number of element present in an array
+console.log("length = ", arr1.length);
+
+// 2. Sort
+console.log("sort = ", arr1.sort());
