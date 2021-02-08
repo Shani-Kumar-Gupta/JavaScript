1fe300db0340cb40129b6fea54409a040e0951bb
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-08 16:11:26 +0530

    JavaScript: Hoisting done with example

---
+++ Core-JavaScript/hoisting.js
@@ -0,0 +1,24 @@
+getUser();
+getName();
+console.log(n);
+console.log(getUser);
+
+var n = 7;
+
+function getUser(){
+    console.log("Hello JavaScript");
+}
+
+/* Behaves like variable*/
+
+var getName2 = function (){
+    console.log("getName2");
+}
+
+var getName = () => {
+    console.log("Shani");
+}
+
+// getUser();
+// console.log(n);
+// console.log(getUser);
