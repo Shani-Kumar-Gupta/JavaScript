49501eb51df96e424d5becd938ab0b1cdd6fe27b
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-10 15:31:43 +0530

    undefined and not defined

---
+++ Core-JavaScript/undefinedNotdefined.js
@@ -0,0 +1,9 @@
+// undefined and not defined
+console.log(a); // undefined
+var a;
+console.log(a);
+a = 10;
+console.log(a);
+a = "hello JS";
+console.log(a);
+console.log(n); // Not defined: Referrence Error
