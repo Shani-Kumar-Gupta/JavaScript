d5052f051990b0f08dc83beb02617203a43f71bc
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-11 23:57:03 +0530

    Block | Block Scope | Shadowing

--- Core-JavaScript/letvsconst.js
+++ Core-JavaScript/letvsconst.js
@@ -17,3 +17,69 @@ console.log(c);
 // e = 20;
 // console.log(e); // Type Error
 var b = 100;
+
+// Block Scope and Shadowing
+// What is Block
+{
+    // Compound Statements
+    var a = 10;
+    console.log(a); 
+}
+
+// Whenever we want to use multiple statement as s single result we have to use block
+// Example:::::::
+if(true) true;
+if (true) {
+    // Multiple Statement
+}
+
+// Block Scope: What all variable and function we can use or axcess inside a particular block
+// Example :::::::
+{
+    var x = 10; // Global Scope
+    let y = 20; // Block Scope
+    const z = 30; // Block Scope
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+console.log(x);
+console.log(y);
+console.log(z); // What is expected output??
+
+// Shadowing
+var m = 100;
+let n = 200;
+const p = 300;
+{
+    var m = 10;
+    let n = 20;
+    const p = 30;
+    console.log(m);
+    console.log(n);
+    console.log(p);
+}
+console.log(m);
+console.log(n);
+console.log(p);
+
+const s = 100;
+function x() {
+    const s = 20;
+    console.log(s);
+}
+x();
+console.log(s);
+
+// INvalid Shadowing
+let as = 20;
+
+// Global Space, Local Space , Script, Block Space 
+const v = 20;
+{
+    const v = 40;
+    {
+        const v = 50;
+        console.log(v);
+    }
+}
