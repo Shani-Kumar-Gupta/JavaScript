3de20d09a3219722d35c3eee1f9fb458cafc267c
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-13 13:12:52 +0530

    typescript duplicate function name changed

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,8 +1,8 @@
-function greetUser(person: string){
+function greetUser1(person: string){
     return "Hello " + person;
 }
 
-let user = greetUser("Shani Kumar Gupta");
+let user = greetUser1("Shani Kumar Gupta");
 console.log(user);
 
 //let user1 = greetUser([0,1,2]); // Return error TypeError
